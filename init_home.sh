b5899f2a2b6327eec6bb86fa419573e0ae054789
Xiangce Liu <xiangceliu@redhat.com>
2020-01-10 12:35:14 +0800

    zshrc
    Signed-off-by: Xiangce Liu <xiangceliu@redhat.com>

--- init_home.sh
+++ init_home.sh
@@ -1,6 +1,6 @@
 # /usr/bin/bash
 
-LIST=".bashrc .ctags .gitconfig .git-prompt.sh .tmux.conf tmux-session .vimrc"
+LIST=".zshrc .ctags .gitconfig .git-prompt.sh .tmux.conf tmux-session .vimrc"
 
 for i in $LIST; do
     ln -f ./$i ~/$i
